0a196647a3780592d83817db26ebedc2652587b6
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-03-25 23:01:18 +0600

    1002 uri problem

---
+++ 1002.py
@@ -0,0 +1,15 @@
+R=float(input( ))
+r = 3.14159
+A = r*R*R
+print(f"A={A:.4f}")
+
+
+# π = 3.14159
+# R = float(input())
+# A = π * R**2
+# print("A=%.4f"%A)
+
+# R=float(input( ))
+# r = 3.14159
+# A = r*R*R
+# print(round(A,4))
