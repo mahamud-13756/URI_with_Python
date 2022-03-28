362ed69ce4f729d52033138d111765a58a337689
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-03-28 19:44:46 +0600

    1010

---
+++ 1010.py
@@ -0,0 +1,15 @@
+a=float(input())
+aa=int(a)
+b=int(input())
+c=float(input())
+
+a2=float(input())
+ab=int(a2)
+b2=int(input())
+c2=float(input())
+
+d1=b*c
+d2=b2*c2
+
+D=d1+d2
+print(f"VALOR A PAGAR: R$ {D:.2f}")
